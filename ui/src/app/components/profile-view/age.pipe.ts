e181ef4cbaa8d8cbe8ddf1f2936dbab80628d2aa
Sebastian Renner <Sebastian.Renner@ti8m.ch>
2024-08-15 10:15:18 +0200

    ran lint:fix

--- ui/src/app/components/profile-view/age.pipe.ts
+++ ui/src/app/components/profile-view/age.pipe.ts
@@ -1,13 +1,13 @@
-import { Pipe, PipeTransform } from "@angular/core";
-import { differenceInYears } from "date-fns";
+import { Pipe, PipeTransform } from '@angular/core';
+import { differenceInYears } from 'date-fns';
 
 @Pipe({
-  name: "age",
+  name: 'age',
 })
 export class AgePipe implements PipeTransform {
   transform(birthDate?: Date): string {
     if (!birthDate) {
-      return "";
+      return '';
     }
 
     return differenceInYears(new Date(), birthDate).toString();
