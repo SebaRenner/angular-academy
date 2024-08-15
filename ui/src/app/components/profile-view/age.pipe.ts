fcc37c581d053163d105c01629c4d139cc9b025b
Sebastian Renner <Sebastian.Renner@ti8m.ch>
2024-08-15 09:25:09 +0200

    use date-fns differenceInYears instead of loading all of moment.js

--- ui/src/app/components/profile-view/age.pipe.ts
+++ ui/src/app/components/profile-view/age.pipe.ts
@@ -1,15 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import moment from 'moment';
+import { Pipe, PipeTransform } from "@angular/core";
+import { differenceInYears } from "date-fns";
 
 @Pipe({
-  name: 'age',
+  name: "age",
 })
 export class AgePipe implements PipeTransform {
   transform(birthDate?: Date): string {
     if (!birthDate) {
-      return '';
+      return "";
     }
 
-    return moment().diff(birthDate, 'y').toString();
+    return differenceInYears(new Date(), birthDate).toString();
   }
 }
